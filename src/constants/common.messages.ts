41f2a9f86e94b170866d1cda7326990ad15b81c6
achuasokan <achuasokan555@gmail.com>
2026-04-25 10:50:00 +0530

    feat:implemnted the custom message and error messages

---
+++ src/constants/common.messages.ts
@@ -0,0 +1,8 @@
+
+export const COMMON_MESSAGES = {
+    
+    LOGIN_SUCCESSFUL: "Logged in successfully",
+    LOGOUT_SUCCESSFUL: "Logged out successfully",
+    SIGNUP_SUCCESSFUL: "User registered successfully",
+    
+} as const
---
+++ src/constants/errors/common.errors.ts
@@ -0,0 +1,6 @@
+export const COMMON_ERRORS = {
+    UNAUTHORIZED: "Unauthorized Access",
+    FORBIDDEN: "Forbidden Access",
+    SOMETHING_WENT_WRONG: "Something went wrong",
+    SESSION_EXPIRED: "Session Expired",  
+} as const;
