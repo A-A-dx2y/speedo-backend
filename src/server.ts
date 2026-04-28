07a5fe3639abd7e5fac6f3af083918b16aeb981e
achuasokan <achuasokan555@gmail.com>
2026-04-28 15:00:10 +0530

    removed some log in the server

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
-        console.log("MONGO_URI:", process.env.MONGO_URI);
         await connectDB();
 
         const httpServer = http.createServer(app)
